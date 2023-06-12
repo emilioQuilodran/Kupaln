6644a784e69957019b7e91f6c53448b612f316fb
emilio <emilio.lihue.q@gmail.com>
2023-06-12 02:37:24 -0300

    fix...

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,8 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-import useSWR from 'swr'
-
 const inter = Inter({ subsets: ['latin'] })
 
  function RootLayout({ children }) {
-  const { data, error } = useSWR('/api/navigation', fetcher)
-  if (error) return <div>Failed to load</div>
-  if (!data) return <div>Loading...</div>
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
