f2d53bfd14c054a56b9219341c13dd62ccf3e5af
emilio <emilio.lihue.q@gmail.com>
2023-06-14 00:49:19 -0300

    fix

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -2,17 +2,12 @@
 "use client";
 import Header from './Header';
 import utils from '@/app/layout.module.scss'
-import { ErrorBoundary } from "react-error-boundary";
 const Layout = ({children }) => {
     return(
-        <ErrorBoundary fallback={
-            <p>Something went worng</p>
-        }>
-            <>
-                <Header></Header>
-                <div className={`${utils.noMargin}`}>{children}</div>
-            </>
-        </ErrorBoundary>
+        <>
+            <Header></Header>
+            <div className={`${utils.noMargin}`}>{children}</div>
+        </>
     )
 }
 
